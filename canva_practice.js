9ccddaf171d0aacdf232209473f679877bdd03b4
soyaine <soyaine@outlook.com>
2016-08-12 18:14:01 +0800

    update
    first week

---
+++ canva_practice.js
@@ -0,0 +1,12 @@
+/**
+ * Created by songyuting on 2016/8/9.
+ */
+
+function draw() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext('2d');
+    if (context){
+        var ctx = canvas.getContext('2d');
+        var a;
+    }
+}
