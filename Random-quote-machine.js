15373400221f337a47d243029c2ccac0f92a4775
soyaine <soyaine@outlook.com>
2016-10-14 18:59:13 +0800

    add songs
    完成诗句插入部分

--- Random-quote-machine.js
+++ Random-quote-machine.js
@@ -1,3 +1,5 @@
+var songs;
+
 function getRandomColor(){
     var styleEle = document.getElementById("colorStyle");
     var newColor = Math.floor( (Math.random() * 360) );
@@ -8,38 +10,49 @@ function getRandomQuote(){
     //var songJSON = https://github.com/jackeyGao/chinese-poetry/blob/master/json/poet.song.100000.json;
 }
 
-function getSong(){
-
+function getRandomSong(song){
+    if(song){
+        var length = song.length;
+        var randomSong = song[Math.floor( (Math.random() * length) )];
+        if(randomSong.hasOwnProperty("paragraphs")){
+            var quoteContent = document.getElementById("quote-content");
+            var randomLine = randomSong.paragraphs[Math.floor( (Math.random() * randomSong.paragraphs.length) )];
+            quoteContent.innerText = randomLine;
+            quoteContent.attributes[2].value = randomSong.author;
+        }
+    }
 }
 
 function loadJSON(callback) {
-
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'poet.song.100000.json', true); // Replace 'my_data' with the path to your file
+    xobj.open('GET', 'poetSongs.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
             callback(xobj.responseText);
+            //return xobj.responseText;
         }
     };
     xobj.send(null);
 }
 
+function loadSongs(){
+    return  loadJSON(function(response){
+        songs = JSON.parse(response);
+        return songs;
+    });
+}
 
 function init(){
     //getSong();
+    var song = loadSongs();
     getRandomColor();
-    getRandomQuote();
+    getRandomSong(songs);
     var nextBtn = document.getElementById("quote-next");
-    nextBtn.addEventListener('click', getRandomColor, false);
-    loadJSON(function(response) {
-        // Parse JSON string into object
-        var actual_JSON = JSON.parse(response);
-        var quoteContent = document.getElementById("quote-content");
-        quoteContent.innerText = actual_JSON[0].paragraphs[0];
-        alert(actual_JSON);
-    });
+    nextBtn.addEventListener('click', function(){
+        getRandomSong(songs);
+        getRandomColor();
+    }, false);
 }
 
 init();
