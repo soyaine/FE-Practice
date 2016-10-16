f438d3bdf099461f88322b1b1f20c9d58f66f1ec
soyaine <soyaine@outlook.com>
2016-10-16 22:25:47 +0800

    换了诗的数据，美化样式，增加翻译

--- Random-poetry-machine.js
+++ Random-poetry-machine.js
@@ -14,27 +14,45 @@ function getRandomSong(song){
     if(song){
         var length = song.length;
         var randomSong = song[Math.floor( (Math.random() * length) )];
-        if(randomSong.hasOwnProperty("paragraphs")){
-            var quoteContent = document.getElementById("quote-content");
-            var randomLine = randomSong.paragraphs[Math.floor( (Math.random() * randomSong.paragraphs.length) )];
-            // 窄屏幕将诗句换行
-            var viewHeight = quoteContent.offsetParent.clientHeight;
-            var viewWidth = quoteContent.offsetParent.clientWidth;
-            var width = document.body.clientWidth;
-            var height = document.body.clientHeight;
-            if( (viewWidth < 1140 && viewWidth/viewHeight > 1) || (viewHeight < 670 && viewWidth/viewHeight < 1) ){
-                randomLine = randomLine.replace(/，/g, "，\n");
-            }
-            quoteContent.innerText = randomLine;
-            quoteContent.attributes[2].value = randomSong.author;
+
+        //// for poertySongs.json
+        //if(randomSong.hasOwnProperty("paragraphs")){
+        //    var quoteContent = document.getElementById("quote-content");
+        //    var randomLine = randomSong.paragraphs[Math.floor( (Math.random() * randomSong.paragraphs.length) )];
+        //    // 窄屏幕将诗句换行
+        //    var viewHeight = quoteContent.offsetParent.clientHeight;
+        //    var viewWidth = quoteContent.offsetParent.clientWidth;
+        //    var width = document.body.clientWidth;
+        //    var height = document.body.clientHeight;
+        //    if( (viewWidth < 1140 && viewWidth/viewHeight > 1) || (viewHeight < 670 && viewWidth/viewHeight < 1) ){
+        //        randomLine = randomLine.replace(/，/g, "，\n");
+        //    }
+        //    quoteContent.innerText = randomLine;
+        //    quoteContent.attributes[2].value = randomSong.author;
+        //}
+
+        // for TangPoetryCut.json
+        var quoteContent = document.getElementById("quote-content");
+        var randomLine = randomSong["detail_text"];
+        randomLine = randomLine.replace(/。/, "。\n");
+        // 窄屏幕将诗句换行
+        var viewHeight = quoteContent.offsetParent.clientHeight;
+        var viewWidth = quoteContent.offsetParent.clientWidth;
+        if( (viewWidth < 700 && viewWidth/viewHeight > 1) || (viewHeight < 670 && viewWidth/viewHeight < 1) ){
+            randomLine = randomLine.replace(/，/g, "，\n");
         }
+        quoteContent.innerText = randomLine;
+        quoteContent.attributes[2].value = randomSong["detail_author"][0];
+        var quoteTrans = document.getElementById("quote-trans");
+        quoteTrans.innerText = randomSong["detail_translate_text"].join("\n");
     }
 }
 
 function loadJSON(callback) {
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'poetSongs.json', true);
+    //xobj.open('GET', 'poetSongs.json', true); //繁体诗
+    xobj.open('GET', 'TangPoetryCut.json', true);
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             callback(xobj.responseText);
@@ -60,6 +78,11 @@ function init(){
     nextBtn.addEventListener('click', function(){
         getRandomSong(songs);
         getRandomColor();
+    }, false);
+    var detailBtn = document.getElementById("quote-detail");
+    detailBtn.addEventListener('click', function(){
+        var quoteTrans = document.getElementById("quote-trans");
+        quoteTrans.classList.toggle("hidden");
     }, false);
 }
 
